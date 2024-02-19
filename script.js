9603e192fb2a339d36c99629d11f6bfee336c4ab
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-02-19 12:02:56 +0530

    use toggle class for animation

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.to(".square", {
-    x: 700,
+    // x: 700,
     duration: 2,
     scrollTrigger: {
         trigger: ".square",
-        start: "top 30%",
-        markers: true
+        start: "top 30%", 
+        end: "center 20%",
+        markers: true,
+        toggleClass: "red"
     }
 })
 
@@ -14,26 +16,5 @@ gsap.to(".square", {
 
 
 /* 
-Trigger the animation when element comes halfway in the viewport. 
-
-start and end properties. 
-
-start: 400: 400px from bottom of view port
-when square element crosses 400px from bottom starts animation. 
-
-start: "top center"
-Means when top portion of element reaches the center of view port starts to animate
-
-start: "top 30%",
-Means when top portion of element reaches the 30% of view port from upside, starts to animate
-
-
-markers: true; 
-markers: {
-            startColor: 'purple',
-            endColor: 'fuchsia',
-            fontSize: '4rem',
-            indent: 100 //Moves the markers position from right
-        }
-        
+Toggle Class:         
 */
