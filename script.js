5d4fa02c1a11b1506842ddd8ec0223ac74282049
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-02-21 17:16:40 +0530

    usage of timeline in gsap

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".box",{
-    x: 500, duration: 2
-})
+// gsap.to(".box",{
+//     x: 500, duration: 2
+// })
 
-gsap.to(".box",{
-    y: 200, duration: 3, delay: 2
-})
+// gsap.to(".box",{
+//     y: 200, duration: 3, delay: 2
+// })
 
-gsap.to(".box",{
-    x: 0, duration: 2, delay: 5
-})
+// gsap.to(".box",{
+//     x: 0, duration: 2, delay: 5
+// })
+
+
+const tl = gsap.timeline();
+
+tl.to(".box", { x: 500, duration: 2 })
+    .to(".box", { y: 200, duration: 3 })
+    .to(".box", { x: 0, duration: 2 })
 
 
 
@@ -20,7 +27,8 @@ tutorial- 6:
 https://www.youtube.com/watch?v=-qQhTxTtpeQ&list=PLMPgoZdlPumexxtvuPUB3TY7LExI1N_Xp&index=6
 
 GSAP TimeLine: 
+if we change the duration of one animation, then we have to change the delay of every animation. 
 
-
+using gsap timeline we can resovle the issue. 
 
 */
