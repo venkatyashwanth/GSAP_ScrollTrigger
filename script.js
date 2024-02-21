d3b74719a538791ad9bcaae639cd77f3dc3be3c2
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-02-21 17:10:26 +0530

    purpose of time line in gsap

--- script.js
+++ script.js
@@ -1,34 +1,26 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".square", {
-    // x: 700,
-    duration: 3,
-    scrollTrigger: {
-        trigger: ".square2",
-        start: "top 60%", 
-        end: "top 40%",
-        scrub: true,
-        toggleActions: 'restart pause resume complete',
-        pin: ".square",
-        pinSpacing: true,
-        markers: {
-            startColor: "purple",
-            endColor: "fuchsia",
-            fontSize: "2rem"
-        },
-    }
+gsap.to(".box",{
+    x: 500, duration: 2
+})
+
+gsap.to(".box",{
+    y: 200, duration: 3, delay: 2
+})
+
+gsap.to(".box",{
+    x: 0, duration: 2, delay: 5
 })
 
 
 
 
 /* 
-tutorial- 5: 
-https://www.youtube.com/watch?v=FH0B3Gdm0P0&list=PLMPgoZdlPumexxtvuPUB3TY7LExI1N_Xp&index=5
+tutorial- 6: 
+https://www.youtube.com/watch?v=-qQhTxTtpeQ&list=PLMPgoZdlPumexxtvuPUB3TY7LExI1N_Xp&index=6
 
-pin property: 
-Target element sticks at the start.
+GSAP TimeLine: 
 
-        pinSpacing: false/true,
+
 
 */
